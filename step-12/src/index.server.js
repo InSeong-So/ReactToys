2bd10d02b76ce6b17af296e5566820bf43399e80
InSeong-So <goflvhxj2547@gmail.com>
2021-11-04 21:17:06 +0900

    💻 step-11 새로 추가 : 서버 사이드 렌더링 4(asset-manifest.json)

--- step-12/src/index.server.js
+++ step-12/src/index.server.js
@@ -12,6 +12,46 @@ import ReactDOMServer from 'react-dom/server';
 import express from 'express';
 import { StaticRouter } from 'react-router-dom';
 import App from './App';
+import path from 'path';
+import fs from 'fs';
+
+// asset-manifest.json에서 파일 경로들을 조회
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf-8'),
+);
+
+const chunks = Object.keys(manifest.files)
+  .filter(key => /chunk\.js$/.exec(key))  // chunk.js로 끝나는 '키'를 찾아서
+  .map(key => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
+  .join(''); // 합칩니다.
+
+const createPage = (root) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="ko">
+    <head>
+      <meta charset="utf-8" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <meta name="theme-color" content="#000000" />
+      <title>React ServerSideRendering App</title>
+      <link href="${manifest.files['main.css']}" rel="stylesheet" />
+    </head>
+    <body>
+      <noscript>앱을 실행하려면 자바스크립트가 필요합니다.</noscript>
+      <div id="root">
+        ${root}
+      </div>
+      <script src="${manifest.files['runtime-main.js']}"></script>
+      ${chunks}
+      <script src="${manifest.files['main.js']}"></script>
+    </body>
+    </html>
+  `;
+};
 
 const app = express();
 
@@ -26,9 +66,14 @@ const serverRender = (req, res, next) => {
   );
 
   const root = ReactDOMServer.renderToString(jsx);  // 렌더링을 하고
-  res.send(root); // 클라이언트에 결과물 응답
+  res.send(createPage(root)); // 클라이언트에 결과물 응답
 };
 
+const server = express.static(path.resolve('./build'), {
+  index: false, // "/" 경로에서 index.html을 보여주지 않도록 설정
+});
+
+app.use(server);  // 순서가 중요한데, 반드시 serverRender 전에 위치해야 합니다.
 app.use(serverRender);
 
 app.listen(5000, () => {
