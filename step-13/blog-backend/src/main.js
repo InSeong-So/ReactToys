0f69303ce805960685edff4a4b1c484365c27f5a
InSeong-So <goflvhxj2547@gmail.com>
2021-11-05 11:26:18 +0900

    💻 step-13 새로 추가 중 : mongoDB 연결 및 esm 적용

---
+++ step-13/blog-backend/src/main.js
@@ -0,0 +1,45 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+
+import api from './api';
+
+require('dotenv').config();
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// router.get('/about/:name?', ctx => {
+//   const { name } = ctx.params;
+//   ctx.body = name ? `${name}의 소개` : '소개';
+// });
+
+// router.get('/posts', ctx => {
+//   const { id } = ctx.query;
+//   ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+// });
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+  console.log(`Server has been http://localhost:${PORT || 5500} started!`);
+});
