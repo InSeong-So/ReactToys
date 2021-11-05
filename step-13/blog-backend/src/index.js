4c2278b3fe641ca383f5a151f27d8f6873fa56a1
InSeong-So <goflvhxj2547@gmail.com>
2021-11-05 13:49:20 +0900

    💻 step-13 새로 추가 중 : User 스키마, 모델 구현과 검증

--- step-13/blog-backend/src/index.js
+++ step-13/blog-backend/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 // 이 파일에서만 no-global-assign ESLint 옵션 비활성화
 /* eslint-disable no-global-assign */
 
---
+++ step-13/blog-backend/src/models/user.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+// 인스턴스 메서드는 화살표 함수를 사용하면 문서 인스턴스에 접근하지 못합니다.
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result;
+};
+
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
+};
+
+UserSchema.methods.serialize = function () {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+};
+
+const User = mongoose.model('User', UserSchema);
+export default User;
