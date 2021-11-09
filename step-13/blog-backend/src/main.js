23e86c84bc809561e1224a7085930fd5c94cd675
InSeong-So <goflvhxj2547@gmail.com>
2021-11-09 14:22:50 +0900

    💻 step-13 완료

--- step-13/blog-backend/src/main.js
+++ step-13/blog-backend/src/main.js
@@ -3,11 +3,14 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
-import api from './api';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 import jwtMiddelware from './lib/jwtMiddleware';
-// import createFakeData from './createFakeData';
+import api from './api';
 
 require('dotenv').config();
+// import createFakeData from './createFakeData';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -43,6 +46,15 @@ app.use(jwtMiddelware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  // Not Found이고 주소가 /api로 시작하지 않으면
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server has been http://localhost:${PORT || 5500} started!`);
